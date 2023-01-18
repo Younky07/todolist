648ce628e9affa2b045e07cd87d9a3a89d6105cb
Black07Jack <Black07Jack@users.noreply.github.com>
2023-01-18 17:13:13 +0800

    Menambhakan loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todolist/todolist.dart';
 
@@ -12,14 +13,55 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Todo List',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Color.fromARGB(255, 0, 0, 0),
-        primarySwatch: Colors.orange,
-      ),
-      home: TodoListPage(),
-    );
+    return FutureBuilder(
+        future: null,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              body: Center(
+                child: Text(snapshot.error.toString()),
+              ),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState) {}
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Todo List',
+            theme: ThemeData(
+              scaffoldBackgroundColor: Color.fromARGB(255, 0, 0, 0),
+              primarySwatch: Colors.orange,
+            ),
+            home: TodoListPage(),
+          );
+        });
   }
 }
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//       future: Firebase.initializeApp(),
+//       builder: (context, snapshot) {
+//         if (snapshot.hasError) {
+//           return Scaffold(
+//             body: Center(child: Text(snapshot.error.toString())),
+//           );
+//         }
+//         // if (snapshot.connectionState == ConnectionState.waiting) {
+//         //   return Loading();
+//         // }
+//         return MaterialApp(
+//           debugShowCheckedModeBanner: false,
+//           home: TodoListPage(),
+//           theme: ThemeData(
+//             scaffoldBackgroundColor: Color.fromARGB(255, 0, 0, 0),
+//             primarySwatch: Colors.orange,
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
