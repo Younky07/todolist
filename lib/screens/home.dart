17699121eca87037c1a31ce77b106bdd566cef34
Black07Jack <Black07Jack@users.noreply.github.com>
2023-01-18 21:39:23 +0800

    menambhakan login

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -94,7 +94,7 @@ class _HomeState extends State<Home> {
                   child: TextField(
                     controller: _todoController,
                     decoration: InputDecoration(
-                        hintText: 'Add a new todo item',
+                        hintText: 'Add a new todo list',
                         border: InputBorder.none),
                   ),
                 ),
